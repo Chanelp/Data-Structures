dad1bac8e1e60933c0b9decbd119aa12f8cfff1f
Chanelp <mariannissanchez27@gmail.com>
2023-01-12 15:20:41 -0400

    Dequeue

---
+++ 4-Queue.js
@@ -0,0 +1,62 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  peek() {
+    return this.first;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (!this.length) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  dequeue() {
+    if (!this.first) {
+      return null;
+    }
+
+    if (this.first === this.last) {
+      this.last = null;
+    }
+
+    this.first = this.first.next;
+    this.last--;
+
+    return this;
+  }
+}
+
+const myQueue = new Queue();
+
+myQueue.enqueue(7);
+console.log(myQueue);
+
+myQueue.enqueue(8);
+console.log(myQueue);
+
+myQueue.enqueue(9);
+console.log(myQueue);
+
+console.log(myQueue.peek());
+myQueue.dequeue();
+console.log(myQueue.peek());
