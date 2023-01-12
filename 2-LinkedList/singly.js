dc574f1b58f74a9b0955612d81b6813a55e5f3c9
Chanelp <mariannissanchez27@gmail.com>
2023-01-11 22:41:19 -0400

    remove

--- 2-LinkedList/singly.js
+++ 2-LinkedList/singly.js
@@ -15,85 +15,95 @@
 // };
 
 class Node {
-    constructor(value) {
-      this.value = value;
-      this.next = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
-  
-  class MySinglyLinkedList {
-    constructor(value) {
-      this.head = {
-        value: value,
-        next: null,
-      };
-  
-      this.tail = this.head;
-      this.length = 1;
-    }
-  
-    //METHODS
-    checkNode(node) {
-      if (!node.next) {
-        return node;
-      }
-      return this.checkNode(node.next);
-    }
-  
-    // Agregar nodo a la cola
-    append(value) {
-      const newNode = new Node(value);
-      this.tail.next = newNode;
-      this.tail = newNode;
-      this.length++;
-      return this;
-    }
-  
-    // Agregar nodo a la cabeza
-    prepend(value) {
-      const newNode = new Node(value);
-  
-      newNode.next = this.head;
-      this.head = newNode;
-  
-      this.length++;
-      return this;
-    }
-  
-    insert(index, value) {
-      // Validación
-      if (index >= this.length) {
-        this.append(value);
-      }
-  
-      const newNode = new Node(value);
-      let firstPointer = this.head;
-      let holdingPointer = firstPointer.next;
-  
-      firstPointer.next = newNode;
-      newNode.next = holdingPointer;
-  
-      this.length++;
-  
-      return this;
-    }
-  
-    getTheIndex(index) {
-      let counter = 0;
-      let currentNode = this.head;
-  
-      while (counter !== index) {
-        currentNode = currentNode.next;
-        counter++;
-      }
-  
-      return this;
-    }
+}
+
+class MySinglyLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+
+    this.tail = this.head;
+    this.length = 1;
   }
-  
-  let myLinkedList = new MySinglyLinkedList(1);
-  myLinkedList.append(2);
-  myLinkedList.append(3);
-  console.log(myLinkedList);
-  myLinkedList.prepend(29);
-  console.log(myLinkedList);
+
+  //METHODS
+  checkNode(node) {
+    if (!node.next) {
+      return node;
+    }
+    return this.checkNode(node.next);
+  }
+
+  // Agregar nodo a la cola
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  // Agregar nodo a la cabeza
+  prepend(value) {
+    const newNode = new Node(value);
+
+    newNode.next = this.head;
+    this.head = newNode;
+
+    this.length++;
+    return this;
+  }
+
+  insert(index, value) {
+    // Validación
+    if (index >= this.length) {
+      this.append(value);
+    }
+
+    const newNode = new Node(value);
+    let firstPointer = this.head;
+    let holdingPointer = firstPointer.next;
+
+    firstPointer.next = newNode;
+    newNode.next = holdingPointer;
+
+    this.length++;
+
+    return this;
+  }
+
+  getTheIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+
+    return this;
+  }
+
+  remove(index) {
+    const previousPointer = this.getTheIndex(index - 1);
+    const holdingPointer = this.getTheIndex(index + 1);
+
+    previousPointer.next = holdingPointer;
+
+    this.length--;
+    return this;
+  }
+}
+
+let myLinkedList = new MySinglyLinkedList(1);
+myLinkedList.append(2);
+myLinkedList.append(3);
+console.log(myLinkedList);
+myLinkedList.prepend(29);
+console.log(myLinkedList);
